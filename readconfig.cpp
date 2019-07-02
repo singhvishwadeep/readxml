f78e4c0ac892c206f4b7fd8ed1766368671b8311
Vishwadeep Singh <vsdpsingh@gmail.com>
2019-07-02 11:03:10 +0530

    Create readconfig.cpp

---
+++ readconfig.cpp
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <libxml/parser.h>
+
+ 
+int is_leaf(xmlNode * node)
+{
+  xmlNode * child = node->children;
+  while(child)
+  {
+    if(child->type == XML_ELEMENT_NODE) return 0;
+ 
+    child = child->next;
+  }
+ 
+  return 1;
+}
+ 
+void print_attr(xmlNode * node)
+{
+    while(node)
+    {
+        if(node->type == XML_ELEMENT_NODE)
+        {
+			printf("key(%s):", node->name);
+			if (is_leaf(node))
+			{
+				printf("value1(%s)\n", xmlNodeGetContent(node));
+			}
+			else
+			{
+				// means it had more childs
+				printf("value2(%s)\n", xmlGetProp(node, "id"));
+			}
+        }
+        print_attr(node->children);
+        node = node->next;
+    }
+} 
+
+void fetch_magtek_card (xmlNode * node)
+{
+	printf("key(%s):", node->name);
+	if (is_leaf(node))
+	{
+		printf("value1(%s)\n", xmlNodeGetContent(node));
+	}
+	else
+	{
+		// means it had more childs
+		printf("value2(%s)\n", xmlGetProp(node, "id"));
+	}
+	print_attr(node->children);
+}
+
+void print_xml(xmlNode * node)
+{
+    while(node)
+    {
+        if(node->type == XML_ELEMENT_NODE)
+        {
+			if ((!xmlStrcmp(node->name, (const xmlChar *)"magtek_card")))
+			{
+				printf("Fetching magtek_card properties\n");
+				fetch_magtek_card(node);
+			}
+			if ((!xmlStrcmp(node->name, (const xmlChar *)"jcm_billvalidator")))
+			{
+				printf("Fetching jcm_billvalidator properties\n");
+				fetch_magtek_card(node);
+			}
+			if ((!xmlStrcmp(node->name, (const xmlChar *)"snbc_printer")))
+			{
+				printf("Fetching snbc_printer properties\n");
+				fetch_magtek_card(node);
+			}
+			if ((!xmlStrcmp(node->name, (const xmlChar *)"witek_scanner")))
+			{
+				printf("Fetching witek_scanner properties\n");
+				fetch_magtek_card(node);
+			}
+        }
+		print_xml(node->children);
+        node = node->next;
+    }
+}
+
+
+ 
+int main(int argc, char **argv)
+{
+	xmlDoc *doc = NULL;
+	xmlNode *root_element = NULL;
+
+	doc = xmlReadFile("config.xml", NULL, 0);
+
+	if (doc == NULL) {
+		printf("Could not parse the XML file");
+	}
+
+	root_element = xmlDocGetRootElement(doc);
+	print_xml(root_element);
+	xmlFreeDoc(doc);
+	xmlCleanupParser();
+	return 0;
+}
